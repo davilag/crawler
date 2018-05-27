8e4916459de94b975c69a969936f5e7dda503d1f
David Avila <dag@bluetel.co.uk>
2018-05-27 09:36:25 +0100

    Moving everything to be managed by a main thread

--- main.go
+++ main.go
@@ -1,23 +1,45 @@
 package main
 
 import (
+    "fmt"
     "github.com/davilag/crawler/page"
-    "sync"
+    "github.com/davilag/crawler/utils"
 )
 
 func main() {
-    or := "https://twitter.com/"
+    or := "httpg://davidavila.xyz/"
 
     //We need a thread safe map here.
     var p page.Page
-    var m sync.Map
     p.BaseUrl = or
-    p.UrlMap = m
-    done := make(chan bool)
-    p.RetrieveUrls(done)
+    processing := map[string]bool{or: true}
+    processed := map[string]bool{or: true}
+    urls := make(map[string][]string)
+    // var m sync.Map
+    c := make(chan map[string][]string)
+    go p.RetrieveUrls(c)
+    for len(processing) > 0 {
+        out := <-c
+        for k, v := range out {
+            fmt.Println("storing in urls ", k)
+            urls[k] = v
+            processed[k] = true
+            delete(processing, k)
+            for _, u := range v {
+                url := utils.AppendPath(or, k, u)
+                fmt.Println("Checking in urls ", url)
+                if processed[url] || processing[url] {
+                    fmt.Println("##################NOPE##################NOPE##################NOPE##################NOPE##################NOPE")
+                    continue
+                }
+                var pv page.Page
+                pv.OriginUrl = or
+                pv.BaseUrl = url
+                processing[url] = true
+                go pv.RetrieveUrls(c)
+            }
+        }
+        fmt.Println(processing)
+    }
 
-    // ls := getPageUrls(or, "")
-    // for _, val := range ls {
-    //     fmt.Println(val)
-    // }
 }
--- page/page.go
+++ page/page.go
@@ -5,7 +5,6 @@ import (
     "github.com/davilag/crawler/utils"
     "golang.org/x/net/html"
     "net/http"
-    "sync"
 )
 
 type PageInterface interface {
@@ -15,45 +14,32 @@ type PageInterface interface {
 type Page struct {
     BaseUrl   string
     OriginUrl string
-    Urls      []string
-    UrlMap    sync.Map
-    Pages     []Page
+    // UrlMap    sync.Map
 }
 
-func (p *Page) RetrieveUrls(o chan bool) {
-    done := make(chan bool)
-    go p.retrieveUrl(done)
-    <-done
-    doneChild := make(chan bool)
-    for _, url := range p.Urls {
-        v := utils.AppendPath(p.OriginUrl, p.BaseUrl, url)
-        if _, ok := p.UrlMap.LoadOrStore(v, true); !ok {
-            fmt.Println("We haven't checked this url yet: ", v)
-            var np Page
-            np.OriginUrl = p.OriginUrl
-            np.BaseUrl = v
-            np.UrlMap = p.UrlMap
-            p.Pages = append(p.Pages, np)
-            go np.RetrieveUrls(doneChild)
-        }
+func (p *Page) RetrieveUrls(o chan map[string][]string) {
+    ls, e := p.retrieveUrl()
+    if e != nil {
+        panic(e)
     }
-    <-doneChild
-    o <- true
+    m := make(map[string][]string)
+    fmt.Println(ls)
+    m[p.BaseUrl] = ls
+    o <- m
 }
 
-func (p *Page) retrieveUrl(c chan bool) {
+func (p *Page) retrieveUrl() (ls []string, er error) {
     if p.OriginUrl == "" {
         fmt.Println("We haven't received the origin url")
         p.OriginUrl = p.BaseUrl
     }
-    var ls []string
+
     fmt.Println("Retrieving ", p.BaseUrl)
     r, e := http.Get(p.BaseUrl)
     if e != nil {
         fmt.Println("We have an error")
         fmt.Println(e)
-        c <- false
-        return
+        return ls, e
     }
 
     defer r.Body.Close()
@@ -65,14 +51,12 @@ func (p *Page) retrieveUrl(c chan bool) {
             if t.Data == "a" {
                 val, ok := utils.GetHref(t)
                 if ok && utils.IsValidURL(val) {
+                    fmt.Println(val)
                     ls = append(ls, val)
                 }
             }
         case html.ErrorToken:
-            fmt.Println(ls)
-            p.Urls = ls
-            c <- true
-            return
+            return ls, nil
         }
     }
 }
